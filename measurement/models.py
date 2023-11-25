dab218c00f00f23d148d763c5b932b290ad4b228
t4zdi0Neo <js.ortegar1@uniandes.edu.co>
2023-11-25 18:22:33 -0500

    cabmio2

--- measurement/models.py
+++ measurement/models.py
@@ -4,8 +4,10 @@ from nodo.models import Nodo
 class Measurement(models.Model):
 
     nodo = models.ForeignKey(Nodo, on_delete=models.CASCADE)
-    latitud = models.FloatField(null=True, blank=True, default=None)
-    longitud = models.FloatField(null=True, blank=True, default=None)
+    latitud = models.BigIntegerField(null=True, blank=True, default=None)
+    longitud = models.BigIntegerField(null=True, blank=True, default=None)
     velocidad = models.FloatField(null=True, blank=True, default=None)
     aceleracion = models.FloatField(null=True, blank=True, default=None)
     azimuth = models.FloatField(null=True, blank=True, default=None) 
+    positionDOP = models.PositiveIntegerField(null=True, blank=True, default=None)
+    weekTime = models.BigIntegerField(null=True, blank=True, default=None)
