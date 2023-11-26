000cbe24f09fcd046a6129308d56c8e6bda0c2f6
t4zdi0Neo <js.ortegar1@uniandes.edu.co>
2023-11-25 20:52:21 -0500

    timestamp

--- measurement/logic/logic_measure.py
+++ measurement/logic/logic_measure.py
@@ -10,23 +10,28 @@ def get_measurement_ID(id_pk):
     return measurement
 
 def log_measurement(data):
+    nodo = Nodo.objects.get(pk=data['nodo'])
 
-    nodo =  Nodo.objects.get(pk=data['nodo'])
-    measurement = Measurement(nodo= nodo, 
-                              latitud= data['latitud'], 
-                              longitud= data['longitud'], 
-                              velocidad= data['velocidad'], 
-                              aceleracion= data['aceleracion'],
-                              azimuth= data['azimuth'],
-                              positionDOP= data['positionDOP'],
-                              time= Time(year=data['time']['year'], 
-                                         month=data['time']['month'], 
-                                         day=data['time']['day'], 
-                                         hour=data['time']['hour'], 
-                                         minute=data['time']['minute'], 
-                                         second=data['time']['second']))
+    measurement = Measurement(
+        nodo=nodo,
+        latitud=data['latitud'],
+        longitud=data['longitud'],
+        velocidad=data['velocidad'],
+        aceleracion=data['aceleracion'],
+        azimuth=data['azimuth'],
+        positionDOP=data['positionDOP'],
+
+        year=data['time']['year'],
+        month=data['time']['month'],
+        day=data['time']['day'],
+        hour=data['time']['hour'],
+        minute=data['time']['minute'],
+        second=data['time']['second'],
+    )
+    
+    if measurement.timestamp_components_valid():
+        measurement.timestamp = measurement._create_timestamp()
 
     measurement.save()
-
 
     return measurement
--- measurement/models.py
+++ measurement/models.py
@@ -1,16 +1,8 @@
 from django.db import models
 from nodo.models import Nodo
 
-class Time(models.Model):
-    year = models.PositiveIntegerField(null=True, blank=True, default=None)
-    month = models.PositiveIntegerField(null=True, blank=True, default=None)
-    day = models.PositiveIntegerField(null=True, blank=True, default=None)
-    hour = models.PositiveIntegerField(null=True, blank=True, default=None)
-    minute = models.PositiveIntegerField(null=True, blank=True, default=None)
-    second = models.PositiveIntegerField(null=True, blank=True, default=None)
-
-
 class Measurement(models.Model):
+
     nodo = models.ForeignKey(Nodo, on_delete=models.CASCADE)
     latitud = models.BigIntegerField(null=True, blank=True, default=None)
     longitud = models.BigIntegerField(null=True, blank=True, default=None)
@@ -18,5 +10,26 @@ class Measurement(models.Model):
     aceleracion = models.FloatField(null=True, blank=True, default=None)
     azimuth = models.FloatField(null=True, blank=True, default=None) 
     positionDOP = models.PositiveIntegerField(null=True, blank=True, default=None)
-    time = models.ForeignKey(Time, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    timestamp = models.DateTimeField(null=True, blank=True, default=None)
     
+    def save(self, *args, **kwargs):
+        if self.timestamp_components_valid():
+            self.timestamp = self._create_timestamp()
+        super().save(*args, **kwargs)
+    
+    def timestamp_components_valid(self):
+        return all(
+            getattr(self, component) is not None
+            for component in ['year', 'month', 'day', 'hour', 'minute', 'second']
+        )
+
+    def _create_timestamp(self):
+        from datetime import datetime
+        return datetime(
+            year=self.year,
+            month=self.month,
+            day=self.day,
+            hour=self.hour,
+            minute=self.minute,
+            second=self.second,
+        )
